fb058220b0beae17d505e7c5b688ada0acf89c98
vanredni1 <vanredni1@ITCRS.edu>
2024-03-07 11:47:17 +0100

    Add project files.

---
+++ vezba1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vezba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ime;
+            string prezime;
+
+            Console.WriteLine("Unesite ime:");
+            ime = Console.ReadLine();
+
+            Console.WriteLine("Unesite prezime:");
+            prezime = Console.ReadLine();
+        }
+    }
+}
